4d09949410c9058ba2a8f4f04213c796143e742c
Robin Khilery <96528134+Robinkhilery960@users.noreply.github.com>
2023-01-18 17:01:22 +0000

    productController- getProductById and getAllProduct

--- config/S3.config.js
+++ config/S3.config.js
@@ -2,7 +2,7 @@ import aws from "aws-sdk"
 import config from "./index.js"
 
 const s3=new aws.S3({
-    accessKeyId:config.S3_SECRET_ACCESS_KEY,
+    accessKeyId:config.S3_ACCESS_KEY,
     secretAccessKey:config.S3_SECRET_ACCESS_KEY,
     region:config.S3_REGION
 })
--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -18,71 +18,135 @@ import config from "../config/index.js"
 
 
 export const addProduct = asyncHandler(async (req, res) => {
-    const form = formidable({
-        multiples: true,
-        keepExtensions: true
-    });
+    
+    // create an instance of the form using formidable
+    const form=formidable({
+        multiples:true,
+        keepExtensions:true
+    }) 
 
-    form.parse(req, async function (err, fields, files){
+    // now parse the request stream 
+    form.parse(req,async function (error,fields,files){
         try {
-            if (err) {
-                throw new CustomError(err.message || "Something went wrong", 500)
+            if(error){
+                return res.status(400).json({
+                    status: "Fail",
+                    message: "There was an error parsing the files",
+                    error: error,
+                  })
             }
-            let productId = new Mongoose.Types.ObjectId().toHexString();
-            //console.log(fields, files)
+
+            const productId=new Mongoose.Types.ObjectId();
+            // const productId=new mongoose.Types.ObjectId()
 
             // check for fields
-            if (!fields.name || 
-                !fields.price ||
-                !fields.description ||
-                !fields.collectionId
-                ) {
-                    throw new CustomError("Please fill all details", 500)
+            if(!(fields.name || fields.price || fields.collectionId)){
+                throw new CustomError("Please provide all mandatory fields",500)
             }
 
-            // handling images
-            let imgArrayResp = Promise.all(
-                Object.keys(files).map(async (filekey, index) => {
-                    const element = files[filekey]
+            // upload images 
+            /* 
+            maps will return you an array which contains promises for each call that will be resolved in future 
+            */
+           const imageArray= await Promise.all(
+            Object.keys(files).map(async(fileKey,index)=>{
 
-                    const data = fs.readFileSync(element.filepath)
+                const element=files[fileKey]
+                // read data of that file 
+                const data=fs.readFileSync(element.filepath)
 
-                    const upload = await s3FileUpload({
-                        bucketName: config.S3_BUCKET_NAME,
-                        key: `products/${productId}/photo_${index + 1}.png`,
-                        body: data,
-                        contentType: element.mimetype
-                    })
-                    return {
-                        secure_url: upload.Location
-                    }
+                // now upload file to aws 
+                const upload=await s3FileUpload({
+                    bucketName:config.S3_BUCKET,
+                    key:`products/${productId}/photo_${index+1}.png`,
+                    body:data,
+                    contentType:element.mimetype
                 })
-            )
 
-            let imgArray = await imgArrayResp;
+                return {
+                    secure_url:upload.Location
+                }
+           })
+           )
+            
+           // now create a product in database
 
-            const product = await Product.create({
-                _id: productId,
-                photos: imgArray,
-                ...fields,
-            })
+           const product=new Product.create({
+            _id:productId,
+            photos:imageArray,
+            ...fields
+           })
 
-            if (!product) {
-                throw new CustomError("Product was not created", 400)
-                //remove image
-            }
-            res.status(200).json({
-                success: true,
-                product
-            })
-           
+           //if product is not created 
+           if(!product){
+            return new customError("Product not created ",400)
+           }
+
+           // product is created 
+             res.status(200).json({
+            status: "success",
+            message: "Product  created successfully", 
+          })
+
         } catch (error) {
-            return res.status(500).json({
-                success: false,
-                message: error.message || "Something went wrong"
-            })
+            return res.status(400).json({
+                status: "Fail",
+                message: "There was an error while uploading  the files",
+                error: error,
+              })
+            
         }
+    })
+
+     
+})
+
+
+/**********************************************************
+ * @GET_ALL_PRODUCT
+ * @route https://localhost:5000/api/product
+ * @description Controller used for getting all products details
+ * @description User and admin can get all the prducts
+ * @returns Products Object
+ *********************************************************/
+
+export const getAllProduct=asyncHandler(async(req, res)=>{
+
+    const products= await Product.find()
+
+    if(!products){
+        throw new customError("No product found",404)
+    }
+
+    res.status(200).json({
+        status:"Success",
+        message:"All products are fatched",
+        products
     })
 })
 
- 
+/**********************************************************
+ * @GET_PRODUCT_BY_ID
+ * @route https://localhost:5000/api/product
+ * @description Controller used for getting single product details
+ * @description User and admin can get single product details
+ * @returns Product Object
+ *********************************************************/
+
+export const getProductById= asyncHandler(async(req, res)=>{
+    // get productId from the front end
+    const {productId}=req.params
+    
+    const product=await Product.findById(productId)
+
+    if(!product){
+        throw new customError("Product not  found",404)
+    }
+
+    res.status(200).json({
+        status:"Success",
+        message:"Product found successfully ",
+        product
+    })
+
+})
